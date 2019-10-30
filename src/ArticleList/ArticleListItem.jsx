2779e05bf5d82c810ec885c8469fe0d6ba56e395
Liu Jiaxin <liu.jiaxin2@husky.neu.edu>
2019-10-30 00:39:05 -0700

    assignment 8

--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -6,13 +6,18 @@ import styles from "./ArticleListItem.module.css";
 const ArticleListItem = props => {
     return (
             <section>
+                <div className={styles.content_wrapper}>
                 <ArticleImage title={props.article.title} url={props.article.image._url}/>
                 <h1 className={styles.title}>{props.article.title}</h1>
                 <p>{props.article.shortText}</p>
                 <p>
                     <time className={styles.time} dateTime={props.article.pubYear}>{props.article.pubDate}</time>
-                    <SlugButton buttonText={props.article.author} slug={props.article.slug}></SlugButton>
+
                 </p>
+                </div>
+                <SlugButton buttonText={props.article.author} slug={props.article.slug}></SlugButton>
+
+
              </section>
     )
 }
